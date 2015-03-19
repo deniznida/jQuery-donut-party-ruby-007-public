de68b2e1aab2c8d891bae0aa2d2d6e49791f5f10
deniznida <denizunat@hotmail.com>
2015-03-19 15:05:29 -0400

    done

--- js/donut-counter.js
+++ js/donut-counter.js
@@ -3,3 +3,46 @@ $(function(){
 });
 
 // your code here
+function DonutCounter(){
+	var num_of_guests = 0;
+	var num_of_donuts = 0;
+
+	$("#guests .add").click(function(){
+		num_of_guests++;
+		$('.guest-count').html(num_of_guests);
+	});
+
+	$("#guests .remove").click(function(){
+		if (num_of_guests > 0){
+			num_of_guests--;
+			$('.guest-count').html(num_of_guests);
+		}
+		
+	});
+
+	$("#donuts .add").click(function(){
+		num_of_donuts++;
+		$('.donut-count').html(num_of_donuts);
+	});
+
+	$("#donuts .remove").click(function(){
+		if (num_of_donuts > 0){
+			num_of_donuts--;
+			$('.donut-count').html(num_of_donuts);
+		}	
+	});
+
+	$("#check").click(function() {
+		if (num_of_guests === 0){
+			$("#status").html('Invite someone you jerk!');
+		} else if (num_of_guests > num_of_donuts){
+			$("#status").html('Guests are sad :(');
+		} else if (num_of_guests > num_of_donuts){
+			$("#status").html('Guests are sad :(');
+		} else if ((num_of_donuts/num_of_guests) > 3){
+			$("#status").html('ARE YOU TRYING TO KILL US ALL!?');
+		} else {
+			$("#status").html('Guests are having a blast!');
+		}
+	});
+}
